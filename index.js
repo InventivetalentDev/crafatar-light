57d8d4e6f2f344af92a8fec1445d2c22e160f186
Haylee Schäfer <haylee@inventivetalent.org>
2024-10-21 16:45:07 +0200

    add custom prefix env option

--- index.js
+++ index.js
@@ -2,15 +2,31 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 80
 
-const render = require("./renders")
+let allowedPrefixes = [
+    'http://textures.minecraft.net/texture/',
+    'https://textures.minecraft.net/texture/'
+];
+if (process.env.ALLOWED_URL_PREFIXES) {
+    allowedPrefixes = process.env.ALLOWED_URL_PREFIXES.split(',');
+}
+
+const render = require("./renders");
 
 app.get('/render', (req, res) => {
     let start = Date.now();
     let url = req.query['url'];
-    if (!url || !url.startsWith('http://textures.minecraft.net/texture/') && !url.startsWith('https://textures.minecraft.net/texture/')) {
+    if (!url) {
         res.status(400).end();
         return;
     }
+    let allowed = false;
+    for (let prefix of allowedPrefixes) {
+        if (url.startsWith(prefix)) {
+            allowed = true;
+            break;
+        }
+    }
+
     try {
         render.draw_model(undefined, url, parseInt(req.query['scale'] || '10'), req.query['overlay'] === 'true', req.query['body'] === 'true', req.query['slim'] === 'true', (err, img) => {
             if (err) {
